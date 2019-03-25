50787d75c8ed1323d9244a87e35d7eeed60759cc
Kamaleeswary <kamaleeanbu@gmail.com>
2019-03-25 15:31:01 +0530

    new bugs fixed

--- src/app/client/src/app/modules/core/components/main-header/main-header.component.ts
+++ src/app/client/src/app/modules/core/components/main-header/main-header.component.ts
@@ -171,6 +171,8 @@ export class MainHeaderComponent implements OnInit, OnDestroy {
         }
       });
     this.setInteractEventData();
+        this.selectToGo();
+
   }
 
   getCacheLanguage() {
@@ -271,5 +273,18 @@ export class MainHeaderComponent implements OnInit, OnDestroy {
     if ( value === 'contact') {
     this.router.navigate(['contactUs']);
     }
+}
+selectToGo() {
+  console.log('this.userProfile.userRoles',this.userProfile)
+  for( let i = this.userProfile; i < this.userProfile; i++  ){
+    if(i === 'TEACHER_BADGE_ISSUER' && i === 'ORG_ADMIN'){
+      this.router.navigate(['/resources']);
+    }
+    if(i === 'CONTENT_CREATOR'){
+      this.router.navigate(['/myassets']);
+    }
+  }
+  
+});
 }
 }
