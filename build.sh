60a10217684dd409e56d12eb782a2817a7dc8129
societal-platform <48090249+societal-platform@users.noreply.github.com>
2019-03-27 14:06:52 +0530

    updated

--- build.sh
+++ build.sh
@@ -11,8 +11,7 @@ org=$(e "${m}" "org")
 name=$(e "${m}" "name")
 version=$(e "${m}" "version")
 
-#docker build -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build . 
-docker build --network=host -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build .
+docker build -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build . 
 docker run --name=${name}-${version}-build ${org}/${name}:${version}-build
 containerid=$(docker ps -aqf "name=${name}-${version}-build")
 rm -rf ./dist
