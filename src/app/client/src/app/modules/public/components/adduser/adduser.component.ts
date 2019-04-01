540976fec34db49d4ac4321b5144e07d5456052b
Kamaleeswary <kamaleeanbu@gmail.com>
2019-04-01 17:25:40 +0530

    code of sharing routes to new page and title is fixed

--- src/app/client/src/app/modules/public/components/adduser/adduser.component.ts
+++ src/app/client/src/app/modules/public/components/adduser/adduser.component.ts
@@ -102,15 +102,12 @@ export class AdduserComponent implements OnInit {
 
             this.toasterService.success('user created successfully');
           }, (err) => {
-            this.toasterService.error(err);
+            console.log('err', err);
+            this.toasterService.error(err.error.params.errmsg);
           });
-
-          // if(this.success) {
-          //   console.log('if is working');
-          //   this.updateUser(this.userId,'CONTENT_CREATOR',this.orgId);
-          // }
         }, (err) => {
-          this.toasterService.error(err);
+          console.log('err', err);
+          this.toasterService.error(err.error.params.errmsg);
           // this.goBackToCoursePage();
         }
 
@@ -197,8 +194,8 @@ export class AdduserComponent implements OnInit {
           this.updateUser(this.userId, 'CONTENT_CREATOR', this.orgId);
         }
        }, (err) => {
-         console.log(err);
-       });
+        this.toasterService.error(err.error.params.errmsg);
+      });
   }
   addmemeber(orgId , userId) {
     console.log('inside member func', userId, orgId);
@@ -247,7 +244,7 @@ export class AdduserComponent implements OnInit {
         this.goBackToCoursePage();
       },
       err => {
-        this.toasterService.error(err);
+        this.toasterService.error(err.error.params.errmsg);
       }
     );
   }
