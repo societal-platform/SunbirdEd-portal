6c3ce0961b26d1a9253bccd72751c6e60cdbaea6
societal-platform <48090249+societal-platform@users.noreply.github.com>
2019-04-02 10:40:41 +0530

    to remove all images from jenkins

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ commit_hash=$1
 org=sunbird
 name=player
 version=$2
-
+sudo docker image prune -a
 docker build -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build . 
 docker run --name=${name}-${version}-build ${org}/${name}:${version}-build
 containerid=$(docker ps -aqf "name=${name}-${version}-build")
